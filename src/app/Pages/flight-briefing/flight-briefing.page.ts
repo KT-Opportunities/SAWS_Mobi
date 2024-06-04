b02eff8e781b7c3e25b69c5154af469da2058e25
mancobakto <150122296+mancobakto@users.noreply.github.com>
2024-06-04 13:59:39 +0200

    clean up flight briefing page

--- src/app/Pages/flight-briefing/flight-briefing.page.ts
+++ src/app/Pages/flight-briefing/flight-briefing.page.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { MatTableDataSource } from '@angular/material/table';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
@@ -8,31 +7,10 @@ import { AuthService } from 'src/app/services/auth.service';
   templateUrl: './flight-briefing.page.html',
   styleUrls: ['./flight-briefing.page.scss'],
 })
-export class FlightBriefingPage implements OnInit {
-  dataSource = new MatTableDataSource<any>();
-  displayedColumns: string[] = ['Flight','departure', 
-  'destination','etd','vfr','edit'];
-  
-
+export class FlightBriefingPage {
   isLogged: boolean = false;
-  isFlight: boolean = true;
-  isexportImport: boolean = false;
-  isdeparture: boolean = false;
-  isDropdownOpen1: boolean = false;
-  isDropdownOpen2: boolean = false;
-  isSharedflight: boolean = false;
-  isEditFlight: boolean = false;
-  isSavedFlight : boolean = false;
-  isSheduleFlight : boolean = false;
-  isSavedTemplate: boolean = false; 
-  selectedOption1: string = 'Select flight';
-  selectedOption2: string = 'Select template';
-// displayedColumns: any;
-
  
   constructor(private router: Router, private authService: AuthService) {}
-
-  ngOnInit() {}
 
   get isLoggedIn(): boolean {
     return this.authService.getIsLoggedIn();
@@ -60,52 +38,6 @@ export class FlightBriefingPage implements OnInit {
 
   NavigateToSheduleFlightReport () {
     this.router.navigate(['/flight-briefing/schedule-flight-report']);
-  }
-
-  
-
-  forecastPage() {
-    this.router.navigate(['/landing-page']);
-  }
-
-  flieghtBrief() {
-    this.isFlight = true;
-   this.isexportImport = false;
-  this.isdeparture = false;
-  this.isDropdownOpen1 = false;
-  this.isSharedflight = false;
-  this.isEditFlight = false;
-  this.isSavedFlight = false;
-  this.isSharedflight = false;
-  this.isSheduleFlight = false;
-  }
-
-  selectOption(option: string, dropdown: string) {
-    if (dropdown === 'dropdown1') {
-      this.selectedOption1 = option;
-     
-    } 
-    if (dropdown === 'dropdown2') {
-      this.selectedOption2 = option;
-     
-    }
-   
-  }
-
-  selectDropdown(dropdown: string) {
-    if (dropdown === 'dropdown1') {
-      this.isDropdownOpen1 = !this.isDropdownOpen1;
-    }
-    if (dropdown === 'dropdown2') {
-      this.isDropdownOpen2 = !this.isDropdownOpen2;
-    }
-    
-   
-}
-  closeAllDropdowns() {
-    this.isDropdownOpen1 = false;
-    this.isDropdownOpen2 = false;
- 
   }
 
   NavigateToLandingPage()
