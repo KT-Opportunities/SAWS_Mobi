f5f3549ad30d796c2ee4815154f5f02488d019fd
Luciainnovate <luciainnovate@gmail.com>
2024-06-20 14:56:32 +0200

    PUSH LOW LEVEL CODE

--- src/app/Pages/domestic/low-level-wind-profile/low-level-wind-profile.component.ts
+++ src/app/Pages/domestic/low-level-wind-profile/low-level-wind-profile.component.ts
@@ -1,11 +1,17 @@
 import { HttpClient } from '@angular/common/http';
-import { ChangeDetectorRef, Component, ElementRef, OnInit } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
+import {
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  OnInit,
+} from '@angular/core';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { DomSanitizer } from '@angular/platform-browser';
 import { Router } from '@angular/router';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { APIService } from 'src/app/services/apis.service';
 import { AuthService } from 'src/app/services/auth.service';
+import { ImageViewrPage } from '../../image-viewr/image-viewr.page';
 
 @Component({
   selector: 'app-low-level-wind-profile',
@@ -13,11 +19,10 @@ import { AuthService } from 'src/app/services/auth.service';
   // styleUrls: ['./low-level-wind-profile.component.scss'],
   styleUrls: ['./../domestic.page.scss'],
 })
-export class LowLevelWindProfileComponent  implements OnInit {
-
+export class LowLevelWindProfileComponent implements OnInit {
   isLogged: boolean = false;
   isLoading: boolean = false;
-
+  lowLevel: any = [];
   constructor(
     private router: Router,
     private authService: AuthService,
@@ -36,8 +41,76 @@ export class LowLevelWindProfileComponent  implements OnInit {
     } else {
       // fetch low level wind profile
     }
-  }
+    this.APIService.GetSourceAviationFolderFilesList('', 72).subscribe(
+      (data) => {
+        console.log('Data received:', data);
 
+        this.lowLevel = data
+          .filter((item: { filename: string }) => item.filename.includes('up'))
+          .map((item: { filename: string }) => item.filename);
+        console.log('lowLevel:', this.lowLevel);
+
+        this.isLoading = false;
+      },
+      (error) => {
+        console.log('Error fetching JSON data:', error);
+        this.isLoading = false;
+      }
+    );
+  }
+  openImageViewer(item: any) {
+    const folderName = '';
+    const fileName = item;
+    console.log('file Name:', fileName);
+    this.isLoading = true;
+
+    this.fetchSecondAPI(folderName, fileName)
+      .then((filetextcontent) => {
+        this.isLoading = false;
+
+        const dialogConfig = new MatDialogConfig();
+        dialogConfig.autoFocus = true;
+        dialogConfig.disableClose = true;
+        dialogConfig.width = '80%';
+        dialogConfig.height = '80%';
+        dialogConfig.data = { filetextcontent };
+
+        const dialogRef = this.dialog.open(ImageViewrPage, dialogConfig);
+
+        dialogRef.afterClosed().subscribe(() => {
+          this.isLoading = false;
+        });
+      })
+      .catch((error) => {
+        console.error('Error fetching file content:', error);
+        this.isLoading = false;
+      });
+  }
+  transformFilename(filename: string): string {
+    if (filename.startsWith('up') && filename.endsWith('.gif')) {
+      return filename.slice(2, -4);
+    }
+    return filename;
+  }
+  fetchSecondAPI(folderName: string, fileName: string): Promise<string> {
+    // Return a promise that resolves with filetextcontent
+    return new Promise<string>((resolve, reject) => {
+      this.APIService.GetAviationFile('', fileName).subscribe(
+        (response) => {
+          // Assuming filetextcontent is obtained from the response
+          const filetextcontent = response.filetextcontent;
+          // Log filetextcontent to verify
+          console.log('File Text Content:', filetextcontent);
+          // Resolve the promise with filetextcontent
+          resolve(filetextcontent);
+        },
+        (error) => {
+          // Reject the promise if there's an error
+          reject(error);
+        }
+      );
+    });
+  }
   get isLoggedIn(): boolean {
     return this.authService.getIsLoggedIn();
   }
@@ -45,5 +118,4 @@ export class LowLevelWindProfileComponent  implements OnInit {
   NavigateToDomestic() {
     this.router.navigate(['/domestic']);
   }
-
 }
