29c032ca850cc18d6a93c8dad99c20d393cf0350
mancobakto <150122296+mancobakto@users.noreply.github.com>
2024-06-25 11:18:09 +0200

    Update login

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@ export const environment = {
   // serverAPI: 'https://localhost:44379/api/',
 
   production: true,
-  serverAPI: 'http://160.119.2s53.130/aviappapi/api/',
+  serverAPI: 'http://160.119.253.130/aviappapi/api/',
 
   // production: false,
   // serverAPI: 'http://qa.j-cred.co.za/aviationappapi/api/',
