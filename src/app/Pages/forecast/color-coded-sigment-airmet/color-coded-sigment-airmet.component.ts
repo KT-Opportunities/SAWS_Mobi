b7644bb430acf35b5888453afa7d224e4fd7afd6
Luciainnovate <luciainnovate@gmail.com>
2024-06-27 08:53:08 +0200

    push popup changes

--- src/app/Pages/forecast/color-coded-sigment-airmet/color-coded-sigment-airmet.component.ts
+++ src/app/Pages/forecast/color-coded-sigment-airmet/color-coded-sigment-airmet.component.ts
@@ -1,9 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, ElementRef, OnInit } from '@angular/core';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { DomSanitizer } from '@angular/platform-browser';
 import { Router } from '@angular/router';
+import { InAppBrowser } from '@awesome-cordova-plugins/in-app-browser/ngx';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { APIService } from 'src/app/services/apis.service';
 import { AuthService } from 'src/app/services/auth.service';
+import { ViewDecodedPage } from '../../view-decoded/view-decoded.page';
 
 @Component({
   selector: 'app-color-coded-sigment-airmet',
@@ -15,19 +18,45 @@ export class ColorCodedSigmentAirmetComponent  implements OnInit {
 
   isLogged: boolean = false;
   loading: boolean = false;
+  isLoading: boolean = true;
+  item: any;
+
 
   constructor(
     private router: Router,
     private authService: AuthService,
-    private apiService: APIService,
-    private sanitizer: DomSanitizer,
+    private elRef: ElementRef,
+    private iab: InAppBrowser,
     private spinner: NgxSpinnerService,
-  ) { }
-
+    private apiService: APIService,
+    private dialog: MatDialog,
+    private cdRef: ChangeDetectorRef
+  ) {}
   ngOnInit() {}
 
   forecastPageNavigation() {
     this.router.navigate(['/forecast']);
   }
+  ImageViewer(item: any) {
+    console.log('file Name:', item);
+    const folderName = 'sigw';
+    const fileName = item;
+    console.log('Folder Name:', folderName);
+    this.isLoading = true;
 
+    this.isLoading = false;
+
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.autoFocus = true;
+    dialogConfig.disableClose = true;
+    dialogConfig.width = '80%';
+    dialogConfig.height = '80%';
+    dialogConfig.data = { item };
+
+    const dialogRef = this.dialog.open(ViewDecodedPage, dialogConfig);
+
+    dialogRef.afterClosed().subscribe(() => {
+      this.isLoading = false;
+    });
+  }
 }
--- src/app/Pages/forecast/trends/trends.component.ts
+++ src/app/Pages/forecast/trends/trends.component.ts
@@ -1,10 +1,11 @@
 import { Component, ElementRef, OnInit } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import { InAppBrowser } from '@awesome-cordova-plugins/in-app-browser/ngx';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { APIService } from 'src/app/services/apis.service';
 import { AuthService } from 'src/app/services/auth.service';
+import { ViewDecodedPage } from '../../view-decoded/view-decoded.page';
 
 @Component({
   selector: 'app-trends',
@@ -68,5 +69,30 @@ export class TrendsComponent  implements OnInit {
       this.isDropdownOpen2 = false;
     }
   }
+  isLoading: boolean = true;
+  item: any;
+  ImageViewer(item: any) {
+    console.log('file Name:', item);
+    const folderName = 'sigw';
+    const fileName = item;
+    console.log('Folder Name:', folderName);
+    this.isLoading = true;
 
+  
+        this.isLoading = false;
+
+        const dialogConfig = new MatDialogConfig();
+        dialogConfig.autoFocus = true;
+        dialogConfig.disableClose = true;
+        dialogConfig.width = '80%';
+        dialogConfig.height = '80%';
+        dialogConfig.data = { item };
+
+        const dialogRef = this.dialog.open(ViewDecodedPage, dialogConfig);
+
+        dialogRef.afterClosed().subscribe(() => {
+          this.isLoading = false;
+        });
+    
+  }
 }
